440a2ce2ed8e86b971d57e5ed18a3062db5c21b7
UpperLand <73501442+UpperLand@users.noreply.github.com>
2021-03-28 13:35:20 +0200

    New Version

---
+++ js/server_ip.js
@@ -0,0 +1,9 @@
+const copy = str => {
+    const el = document.createElement('textarea');
+    el.value = "45.35.63.82:19427";
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert("IP del servidor copiada al portapapeles");
+  };
---
+++ js/server_status.js
@@ -0,0 +1,19 @@
+//based on a pen by @robinselmer
+const url = "https://api.minetools.eu/ping/45.35.63.82/19427";
+
+function getServerData() {
+   $.getJSON(url, function (info) {
+      //data is the JSON string
+      if (info.error) {
+         $('#status_color').css('color', 'red');
+         $('#status_signal').css('background-color', 'red');
+         $('#status_color').html('Offline');
+         return false;
+      } else {
+         $('#status_color').css('color', '#80ff00');
+         $('#status_signal').css('background-color', '#80ff00');
+         $('#status_color').html('Online');
+      }
+   });
+};
+setInterval(getServerData, 1000);
