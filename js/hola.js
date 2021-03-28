4bf5f49909c43974f866209bd1ca3b609833c1cb
UpperLand <73501442+UpperLand@users.noreply.github.com>
2021-03-28 13:37:36 +0200

    Delete hola.js

--- js/hola.js
+++
@@ -1,9 +0,0 @@
-const copy = str => {
-    const el = document.createElement('textarea');
-    el.value = "45.35.63.82:19427";
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    alert("IP del servidor copiada al portapapeles");
-  };
