d2e14c07cdff0da385c2058f59d09f6e96296050
UpperLand <73501442+UpperLand@users.noreply.github.com>
2021-03-30 15:45:03 +0200

    Players list updated
    Added real-time online players list. 
    Now admits diferent clasification of players (STAFF, normal players, etc

--- js/server_status.js
+++ js/server_status.js
@@ -1,10 +1,16 @@
-//based on a pen by @robinselmer
-const url = "https://api.minetools.eu/ping/45.35.63.82/19427";
+/*Staff Members list*/
+const staffMembers = ["AlexHowls", "AndreitaGamer", "Miguel", "Artuloda"];
+/*Format for STAFF tag*/
+const staffTag = "<span class='staff_tag'> (STAFF)</span>"
+/*Server PING URL*/
+const statusUrl = "https://api.minetools.eu/ping/45.35.63.82/19427";
+/*List of online players*/
+var playerList = [];
 
-function getServerData() {
-   $.getJSON(url, function (info) {
+function getServerStatus() {
+   $.getJSON(statusUrl, function (statusInfo) {
       //data is the JSON string
-      if (info.error) {
+      if (statusInfo.error) {
          $('#status_text').css('color', 'red');
          $('#status_text').html('Offline');
          $('#players_num').html(null);
@@ -12,9 +18,14 @@ function getServerData() {
       } else {
          $('#status_text').css('color', '#80ff00');
          $('#status_text').html('Online');
-         $('#players_num').html('Jugadores online: '+ info.players.online);
-         
+         $('#players_num').html('Jugadores online: ' + statusInfo.players.online);
+         var players = statusInfo.players.sample.map(player => "<li id='"+player.name+"'>"+player.name+"</li>");
+         $('#players_list').html(players);
+         for (let y = 0; y < staffMembers.length; y++) {
+            $('#'+staffMembers[y]).append(staffTag);
+         }
       }
    });
-};
-setInterval(getServerData, 2000);
+}
+
+setInterval(getServerStatus, 2000);
