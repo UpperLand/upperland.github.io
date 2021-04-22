752a742a2477f5ad90701cbc416b13e627ba44e1
UpperLand <73501442+UpperLand@users.noreply.github.com>
2021-04-23 01:58:32 +0200

    Add files via upload

--- js/server_status.js
+++ js/server_status.js
@@ -1,20 +1,20 @@
 $(function () {
 
-   /*Owners list*/
-   const owners = ["Artuloda", "miguel"];
-   /*Admins list*/
-   const admins = ["AlexHowls", "andreitagamer"];
+   /*Staff list*/
+   const staff = ["AlexHowls", "Artuloda", "miguel"];
+   /*Streamers list*/
+   const streamers = ["andreitagamer"];
    /*Mods list*/
-   const mods = ["Chiara"];
+   const mods = [];
    /*Helpers list*/
-   const helpers = ["GoPeBeTa20", "TheBismarckcraft"];
+   const helpers = [];
    /*Honor members list*/
-   const honorMembers = ["Yisus"];
+   const honorMembers = [];
 
    /*Format for Owner tag*/
-   const ownerFlag = "<span class='owner_tag'> (OWNER)</span>"
+   const staffFlag = "<span class='owner_tag'> (STAFF)</span>"
    /*Format for Admin tag*/
-   const adminFlag = "<span class='admin_tag'> (ADMIN)</span>"
+   const streamersFlag = "<span class='admin_tag'> (STREAMER)</span>"
    /*Format for Mod tag*/
    const modFlag = "<span class='mod_tag'> (MOD)</span>"
    /*Format for Helper tag*/
@@ -23,17 +23,18 @@ $(function () {
    const honorFlag = "<span class='honor_tag'> (HONOR)</span>"
 
    /*Server PING URL*/
-   const statusUrl = "https://api.minetools.eu/ping/45.35.63.82/19427";
+   const statusUrl = "https://api.minetools.eu/ping/45.35.210.2/25696";
    /*List of online players*/
    var playerList = [];
+   var alarm = 0;
 
    /*This funtion add the rank tags to the names*/
    function rankTags() {
-      for (let y = 0; y < owners.length; y++) {
-         $('#' + owners[y]).append(ownerFlag);
+      for (let y = 0; y < staff.length; y++) {
+         $('#' + staff[y]).append(staffFlag);
       }
-      for (let y = 0; y < admins.length; y++) {
-         $('#' + admins[y]).append(adminFlag);
+      for (let y = 0; y < streamers.length; y++) {
+         $('#' + streamers[y]).append(streamersFlag);
       }
       for (let y = 0; y < mods.length; y++) {
          $('#' + mods[y]).append(modFlag);
@@ -48,28 +49,34 @@ $(function () {
 
    function getServerStatus() {
       $.getJSON(statusUrl, function (statusInfo) {
-         /*Code bellow forces status to appear as OFFLINE (during maintenance tasks etc)*/
+         /*Code bellow forces status to appear as OFFLINE (during maintenance tasks etc)
          $('#status_text').css('color', 'red');
          $('#status_text').html('Offline');
          $('#players_num').html(null);
          playerList = [];
-         return false;
+         return false;*/
 
          /*Normal code for displaying server status*/
          if (statusInfo.error) {
+            alarm = 1;
             $('#status_text').css('color', 'red');
             $('#status_text').html('Offline');
             $('#players_num').html(null);
             playerList = [];
             return false;
          } else {
+            alarm = 0;
             $('#status_text').css('color', '#80ff00');
             $('#status_text').html('Online');
             $('#players_num').html('Jugadores online: ' + statusInfo.players.online);
+            /*Temporary disabled
             var players = statusInfo.players.sample.map(player => "<li id='" + player.name + "'>" + player.name + "</li>");
             $('#players_list').html(players);
-            rankTags();
+            rankTags();*/
          }
+if (alarm === 1) {
+   $('audio#beep')[0].play()
+}
       });
    }
 
